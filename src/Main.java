b8b2fae55a239f2a6d2e1521593da5bb36e9ed12
kayaren <sergazievadil@gmail.com>
2023-02-09 15:06:22 +0600

    hm3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-   double[] num = {1.34,1.44,1.52,-1.88,1.36,-4.54,7.48,2.41,3.78,-2.74,8.45,6.14,-5.75,8.45,-9.68};
+   double[] num = {1.34,1.44,1.52,1.88,1.36,4.54,7.48,-2.41,3.78,2.74,-8.45,6.14,5.75,-8.45,9.68};
     int numCount = 0;
     double allNum = 0.0;
     boolean check = false;
@@ -15,6 +17,18 @@ public class Main {
         }
     }
         System.out.println("среднее арифметическое число" + allNum /numCount);
+        for (int i = 0; i < num.length - 1; i++) {
+            for (int j = 0; j <num.length - i - 1 ; j++) {
+                if (num [j]>num[j+1]){
+                    double temp = num [j];
+                    num[j] = num [j +1];
+                    num [j+1] = temp;
+                }
+
+            }
+            System.out.println(Arrays.toString((num)));
+        }
 
     }
+
 }
